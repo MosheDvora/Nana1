6bf3e87851243fcb22ce307760cb15b0c5022e25
moshe <moshe.dvora@gmail.com>
2022-05-01 22:11:44 +0300

    def days_to_unit

---
+++ days_to_unit.py
@@ -0,0 +1,17 @@
+calcultion_to_unit = 24
+name_of_unit = "hours"
+
+
+def days_to_unit(num_of_days):
+    if  num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calcultion_to_unit} {name_of_unit}"
+    else:
+        return "You enter a negative number"
+
+user_input = input("Hey user enter a number\n")
+user_input_number = int(user_input)
+calculate_value = days_to_unit(user_input_number)
+print(calculate_value)
+
+
+days_to_unit(35)

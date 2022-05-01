8ed1dea682a16589ac1e0507350ec9a0b39ca496
moshe <moshe.dvora@gmail.com>
2022-05-01 22:11:44 +0300

    days_to_unit

---
+++ days_to_unit.py
@@ -0,0 +1,10 @@
+calcultion_to_unit = 24
+name_of_unit = "hours"
+
+
+def days_to_unit(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calcultion_to_unit} {name_of_unit}")
+    print("All Good")
+
+
+days_to_unit(35)

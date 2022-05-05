c21df1bb9819cd924571c96194dc031a23355f9d
moshe <moshe.dvora@gmail.com>
2022-05-05 23:32:38 +0300

    return "You enter a 0 "

--- days_to_unit.py
+++ days_to_unit.py
@@ -5,6 +5,8 @@ name_of_unit = "hours"
 def days_to_unit(num_of_days):
     if  num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calcultion_to_unit} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You enter a 0 "
     else:
         return "You enter a negative number"
 
